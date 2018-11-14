2c4c2ad5e56504e77ffbc212199e7c524f60d3a7
Jacky Zheng <42841563+zheng416@users.noreply.github.com>
2018-11-14 17:43:07 -0500

    Add files via upload

--- ChatServer.java
+++ ChatServer.java
@@ -7,132 +7,135 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 final class ChatServer {
-	private static int uniqueId = 0;
-	private final List<ClientThread> clients = new ArrayList<>();
-	private final int port;
+    private static int uniqueId = 0;
+    private final List<ClientThread> clients = new ArrayList<>();
+    private final int port;
 
 
-	private ChatServer(int port) {
-		this.port = port;
-	}
+    private ChatServer(int port) {
+        this.port = port;
+    }
 
-	/*
-	 * This is what starts the ChatServer.
-	 * Right now it just creates the socketServer and adds a new ClientThread to a list to be handled
-	 */
-	private void start() {
-		try {
-			ServerSocket serverSocket = new ServerSocket(port);
-			while (true) {
-				Socket socket = serverSocket.accept();
-				Runnable r = new ClientThread(socket, uniqueId++);
-				Thread t = new Thread(r);
-				clients.add((ClientThread) r);
-				t.start();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    /*
+     * This is what starts the ChatServer.
+     * Right now it just creates the socketServer and adds a new ClientThread to a list to be handled
+     */
+    private void start() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                Runnable r = new ClientThread(socket, uniqueId++);
+                Thread t = new Thread(r);
+                clients.add((ClientThread) r);
+                t.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    /**
+     *
+     * @param message
+     */
+    private synchronized void broadcast(String message) {
+        for (int i = 0; i < clients.size() ; i++) {
+            clients.get(i).writeMessage(message);
+        }
 
-	/*
-	 *  > java ChatServer
-	 *  > java ChatServer portNumber
-	 *  If the port number is not specified 1500 is used
-	 */
-	public static void main(String[] args) {
-		boolean flag = true;
-		while (flag) {
-			Scanner scan = new Scanner(System.in);
-			String a = scan.nextLine();
-			String[] b = a.split(" ");
-			if (b.length == 2) {
-				flag = false;
-				ChatServer server = new ChatServer(Integer.parseInt(b[1], 10));
-				server.start();
-			} else if (b.length == 1) {
-				flag = false;
-				ChatServer server = new ChatServer(1500);
-				server.start();
-			} else {
-				flag = true;
-			}
-		}
-	}
+        System.out.println(message);
+    }
 
+    private synchronized void remove(int id) {
 
-	/*
-	 * This is a private class inside of the ChatServer
-	 * A new thread will be created to run this every time a new client connects.
-	 */
-	private final class ClientThread implements Runnable {
-		Socket socket;
-		ObjectInputStream sInput;
-		ObjectOutputStream sOutput;
-		int id;
-		String username;
-		ChatMessage cm;
+    }
 
-		private ClientThread(Socket socket, int id) {
-			this.id = id;
-			this.socket = socket;
-			try {
-				sOutput = new ObjectOutputStream(socket.getOutputStream());
-				sInput = new ObjectInputStream(socket.getInputStream());
-				username = (String) sInput.readObject();
-			} catch (IOException | ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-		}
-
-		/*
-		 * This is what the client thread actually runs.
-		 */
-		@Override
-		public void run() {
-			// Read the username sent to you by client
-			try {
-				cm = (ChatMessage) sInput.readObject();
-			} catch (IOException | ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-			System.out.println(username + ": Ping");
+    /*
+     *  > java ChatServer
+     *  > java ChatServer portNumber
+     *  If the port number is not specified 1500 is used
+     */
+    public static void main(String[] args) {
+        ChatServer server = new ChatServer(1500);
+        server.start();
+    }
 
 
-			// Send message back to the client
-			try {
-				sOutput.writeObject("Pong");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		private boolean writeMessage(String msg) {
-			if (clients.size() == 0) {
-				return false;
-			} else {
-				try {
-					sOutput.writeObject(cm);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				return true;
-			}
-		}
-	}
+    /*
+     * This is a private class inside of the ChatServer
+     * A new thread will be created to run this every time a new client connects.
+     */
+    private final class ClientThread implements Runnable {
+        Socket socket;
+        ObjectInputStream sInput;
+        ObjectOutputStream sOutput;
+        int id;
+        String username;
+        ChatMessage cm;
 
-	private synchronized void broadcast(String msg) {
-		for (int i = 0; i < clients.size();i++) {
-			clients.get(i).writeMessage(clients.get(i).cm);
-		}
-	}
-	private void remove(int id) {
+        private ClientThread(Socket socket, int id) {
+            this.id = id;
+            this.socket = socket;
+            try {
+                sOutput = new ObjectOutputStream(socket.getOutputStream());
+                sInput = new ObjectInputStream(socket.getInputStream());
+                username = (String) sInput.readObject();
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
 
-	}
-	private void close() {
+        /**
+         *
+         * @param msg
+         * @return
+         */
+        private boolean writeMessage(String msg) {
+            return false;
+        }
 
-	}
+        /**
+         *
+         */
+        private void close() {
+
+        }
+
+        /*
+         * This is what the client thread actually runs.
+         */
+        @Override
+        public void run() {
+            // Read the username sent to you by client
+
+            if (cm.getType() == 0) {
+                try {
+                    cm = (ChatMessage) sInput.readObject();
+                } catch (IOException | ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(username + ": " + cm.getMessage());
+            } else {
+                System.out.println(username + "disconnected with a LOGOUT message.");
+            }
+            // Send message back to the client
+            if (cm.getType() == 1) {
+                try {
+                    sOutput.writeObject("Server has closed the connection");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            try {
+                sOutput.writeObject("Pong");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
