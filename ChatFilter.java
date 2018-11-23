e51cfa0fa25d8dd3271b918bd16dff26aa1c0f5e
WenxiZhang0718 <42841627+WenxiZhang0718@users.noreply.github.com>
2018-11-22 22:03:03 -0500

    Add files via upload

--- ChatFilter.java
+++ ChatFilter.java
@@ -1,12 +1,33 @@
-
+import java.io.*;
 
 public class ChatFilter {
-
+    String badWords;
     public ChatFilter(String badWordsFileName) {
-
+     this.badWords  = badWordsFileName;
     }
 
     public String filter(String msg) {
+        File bad = new File(badWords);
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(bad));
+            try {
+                String read = in.readLine();
+                while (read != null) {
+                    String re = "";
+                    for (int i = 0; i < read.length(); i++) {
+                        re += "*";
+                    }
+                    msg.replaceAll("(?i)"+read, re);
+                    read = in.readLine();
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         return msg;
     }
 }
